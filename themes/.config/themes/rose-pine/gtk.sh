0d1a3394891dd4474b00332a88bfdb0d24c226c1
ygt-ernsy <yigiterensoy@hotmail.com>
2026-07-18 01:41:18 +0300

    themes: fix rose-pine gtk theme name

--- themes/.config/themes/rose-pine/gtk.sh
+++ themes/.config/themes/rose-pine/gtk.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-gsettings set org.gnome.desktop.interface gtk-theme    "rose-pine-gtk"
-gsettings set org.gnome.desktop.interface color-scheme "default"
+gsettings set org.gnome.desktop.interface gtk-theme    "Rosepine-BL-LB-Dark"
+gsettings set org.gnome.desktop.interface color-scheme "perefer-dark"
 kvantummanager --set "rose-pine-iris"
