feadc576d4345f75b99035d5651c4ff0fc8d316b
ygt-ernsy <yigiterensoy@hotmail.com>
2026-09-09 23:55:56 +0300

    fix: walker gsettings schema issue

--- themes/.config/themes/rose-pine/gtk.sh
+++ themes/.config/themes/rose-pine/gtk.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+# Recover GSETTINGS_SCHEMA_DIR when run outside a login shell (Hyprland, systemd
+# units, keybinds); otherwise gsettings reports "No schemas installed".
+if [ -z "${GSETTINGS_SCHEMA_DIR:-}" ] && [ -r /etc/set-environment ]; then
+    GSETTINGS_SCHEMA_DIR=$(. /etc/set-environment; printf '%s' "$GSETTINGS_SCHEMA_DIR")
+    export GSETTINGS_SCHEMA_DIR
+fi
+
 gsettings set org.gnome.desktop.interface gtk-theme    "Rosepine-BL-LB-Dark"
-gsettings set org.gnome.desktop.interface color-scheme "perefer-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 kvantummanager --set "rose-pine-iris"
--- themes/.config/themes/tokyonight/gtk.sh
+++ themes/.config/themes/tokyonight/gtk.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+# Recover GSETTINGS_SCHEMA_DIR when run outside a login shell (Hyprland, systemd
+# units, keybinds); otherwise gsettings reports "No schemas installed".
+if [ -z "${GSETTINGS_SCHEMA_DIR:-}" ] && [ -r /etc/set-environment ]; then
+    GSETTINGS_SCHEMA_DIR=$(. /etc/set-environment; printf '%s' "$GSETTINGS_SCHEMA_DIR")
+    export GSETTINGS_SCHEMA_DIR
+fi
+
 gsettings set org.gnome.desktop.interface gtk-theme    "Tokyonight-Dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 kvantummanager --set "Kvantum-Tokyo-Night"
